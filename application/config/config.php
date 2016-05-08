7647b1d64b8749d1e25a0f96bacc531d2514f5b6
ricky rimbao <rcrimbz@gmail.com>
2016-05-08 13:00:29 +0800

    changed domain name

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://www.dion.dev';
+$config['base_url'] = 'http://beta-diononline.esy.es/';
 
 /*
 |--------------------------------------------------------------------------
